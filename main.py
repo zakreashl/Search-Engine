0419ea50526fadba8512889fe0d7f47db1ec1bfc
Zakrea <139512367+zakreashl@users.noreply.github.com>
2026-01-21 19:17:05 -0600

    Process data to remove puncuation and stop words

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 import pandas
+import re
+
+stop_words: set = ["a", "to", "the", "is", "in", "and"]
 
 dataset = load_dataset("NeelNanda/wiki-10k")
-
 df = dataset["train"].to_pandas()
 
-print(df.head())
+def process_clean_text(clean_text: list, stop_words: set) -> list:
+    processed = []
+
+    for word in clean_text:
+        if word not in stop_words or len(word) > 2:
+            processed.append(word)
+
+    return processed
+
+
+df["clean_text"] = (
+    df["text"]
+    .str.lower() # Set all letters to lowercase
+    .str.replace('[,.!?\n]', '', regex=True) # Remove punctuation
+    .str.strip() # Remove trailing white space
+    .str.split(" ") # Turn into list of words
+    .apply(process_clean_text, stop_words=stop_words) # process the text in a custom function
+)
+
+dataset["train"] = Dataset.from_pandas(df)
+
+print(dataset["train"].features)
+print(dataset["train"][0]["clean_text"][:300])
